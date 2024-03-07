9d612a950ade15e39ccfefc74a7aaa6a5e20f6ed
fash2110 <66839819+fash2110@users.noreply.github.com>
2024-03-06 20:42:23 -0600

    Add files via upload

--- views.py
+++ views.py
@@ -8,18 +8,16 @@ from .models import Sistema
 from .forms import FormularioInsertar
 import pyodbc
 
-
-
-
+#conn = pyodbc.connect('DSN=DSN_P1;UID=PySQL;PWD=1234')
+#cursor = conn.cursor()
 
 # Create your views here.
 def sistema(request):
     conn = pyodbc.connect('DSN=DSN_P1;UID=PySQL;PWD=1234')
-
     cursor = conn.cursor()
     mymembers = []
-
     cursor.execute("EXEC ListarEmpleados")
+    
     #print(cursor.description[0][0])
     columns = [column[0] for column in cursor.description]
     for row in cursor.fetchall():
@@ -33,32 +31,26 @@ def sistema(request):
 
 def insertar(request):
     conn = pyodbc.connect('DSN=DSN_P1;UID=PySQL;PWD=1234')
-
-    cursor = conn.cursor()
     # if this is a POST request we need to process the form data
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
         form = FormularioInsertar(request.POST)
         # check whether it's valid:
-        print(form.is_valid())
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            print("EXEC InsertarEmpleado " + form.cleaned_data['eNombre'] + " " + str(form.cleaned_data['eSalario']))
-            conn.execute("EXEC InsertarEmpleado '" + form.cleaned_data['eNombre'] + "', " + str(form.cleaned_data['eSalario']))
+            conn = pyodbc.connect('DSN=DSN_P1;UID=PySQL;PWD=1234')
+            cursor = conn.cursor()
+            print("{CALL InsertarEmpleado (?,?)}", (form.cleaned_data['eNombre'], form.cleaned_data['eSalario']))
+            cursor.execute("{CALL InsertarEmpleado (?,?)}", (form.cleaned_data['eNombre'], form.cleaned_data['eSalario']))
+            conn.commit() #NECESARIO para que se inserte un usuario
             # redirect to a new URL:
             return HttpResponseRedirect("/thanks/")
-        print('no')
     # if a GET (or any other method) we'll create a blank form
     else:
         form = FormularioInsertar()
-    conn.close()
     return render(request, "insertar.html", {"form": form})
 
 
 def thanks(request):
-    conn = pyodbc.connect('DSN=DSN_P1;UID=PySQL;PWD=1234')
-
-    cursor = conn.cursor()
     template = loader.get_template('thanks.html')
-    conn.close()
     return HttpResponse(template.render())
